2d8ce1d5a32ae1e5322a98bb706945870a210bf5
LuizClaudioVasconcellos <luizvasconcellosjunior@gmail.com>
2022-01-10 18:55:28 -0300

    Configuração de rota

--- src/route.js
+++ src/route.js
@@ -6,7 +6,9 @@ const route = express.Router();
 route.get('/', (require, response) => response.render("index"));
 route.get('/create-pass', (require, response) => response.render("create-pass"));
 route.get('/room', (require, response) => response.render("room"));
-// route.post('/room/:room/:question/:action', require,response) => response.render("");
+
+//Formato que o formulário de dentro da modal tem que passar a informação
+// route.post('/room/:room/:question/:action')
 
 
 module.exports = route;
